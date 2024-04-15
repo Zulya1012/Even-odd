b063f567aa8ce25c6e338a4a388632b246904528
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-15 22:26:13 +0400

    Update ArrAy.java

--- ArrAy.java
+++ ArrAy.java
@@ -1,38 +1,47 @@
 import java.util.Arrays;
 public class ArrAy {
-    public static void main(String[] args) {
-        int[] originalArray = {7, 2, 4, 1, 3, 5, 6, 8, 2, 10};
+  public static void main(String[] args) {
+    int[] originalArray = {
+      7,
+      2,
+      4,
+      1,
+      3,
+      5,
+      6,
+      8,
+      2,
+      10
+    };
 
-        System.out.println("Original array: " + Arrays.toString(originalArray));
+    System.out.println("Original array: " + Arrays.toString(originalArray));
 
-        partitionArray(originalArray);
+    partitionArray(originalArray);
 
-        System.out.println("After partition the said array becomes: " + Arrays.toString(originalArray));
+    System.out.println("After partition the said array becomes: " + Arrays.toString(originalArray));
+  }
+
+  public static void partitionArray(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+
+    while (left < right) {
+
+      while (arr[left] % 2 == 0 && left < right) {
+        left++;
+      }
+
+      while (arr[right] % 2 != 0 && left < right) {
+        right--;
+      }
+
+      if (left < right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+      }
     }
-
-    public static void partitionArray(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-           
-            while (arr[left] % 2 == 0 && left < right) {
-                left++;
-            }
-
-         
-            while (arr[right] % 2 != 0 && left < right) {
-                right--;
-            }
-
-           
-            if (left < right) {
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-                left++;
-                right--;
-            }
-        }
-    }
+  }
 }
